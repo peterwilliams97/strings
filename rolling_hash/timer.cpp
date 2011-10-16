17437c3f2748b51b85300f50371ab8400dbf5f8a
Peter Williams <peter.williams.97@gmail.com>
2011-10-16 22:49:13 +1100

    cleaned junk out of timer code

--- rolling_hash/timer.cpp
+++ rolling_hash/timer.cpp
@@ -1,13 +1,3 @@
-//#include <intrin.h>
-//
-//unsigned __int64 rdtsc(void)
-//{
-//  return __rdtsc();
-//}
-
-// Always define NOMINMAX before including windows.h to prevent
-// strange errors wherever min() and max() are used in any context.
-#define NOMINMAX
 #include <windows.h>
 #include "timer.h"
 
@@ -25,7 +15,7 @@ void timer_init()
 {
     LARGE_INTEGER freq;
     QueryPerformanceFrequency(&freq);
-    _freq = 1.0f / freq.QuadPart;
+    _freq = 1.0 / freq.QuadPart;
     _time0 = _get_absolute_time();
 }
 
@@ -33,27 +23,4 @@ double get_elapsed_time()
 {
     return _get_absolute_time() - _time0;
 }
-
-#if 0
-#include <sys/stat.h>
-
-#include <sys/time.h>
-#include <sys/types.h
-
-
-
-class ZTimer
-{
-    struct timeval t1, t2;
-public:
-    ZTimer()      { gettimeofday(&t1 ,0); t2 = t1; }
-    void reset()  { gettimeofday(&t1 ,0); t2 = t1;}
-    int elapsed() { return ((t2.tv_sec - t1.tv_sec) * 1000) + ((t2.tv_usec - t1.tv_usec) / 1000); }
-    int split()   { gettimeofday(&t2,0); return elapsed(); }
-};
-
-#endif
-
-
-
 
