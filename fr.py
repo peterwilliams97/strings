b0f28f7284f839e921979173af16eab513dd22e3
peterwilliams97 <peter.williams.97#gmail.com>
2012-04-07 10:37:30 +1000

    Clarified code

--- fr.py
+++ fr.py
@@ -1,9 +1,14 @@
+# Comments, I've made a few. But then again, too few to mention.
 import glob, os, re, sys
 assert len(sys.argv) > 1, 'Usage: python %s <file pattern>' % sys.argv[0]
-corpus = [(fn, int(re.search(r'pages(\d+)', fn).group(1)), file(fn, 'rb').read()) 
-        for fn in [p for p in glob.glob(sys.argv[1]) if not os.path.isdir(p)]]
+files = [fn for fn in glob.glob(sys.argv[1]) if not os.path.isdir(fn)]
+corpus = [(int(re.search(r'pages(\d+)', fn).group(1)), file(fn, 'rb').read()) 
+        for fn in files]
 assert corpus, r'No files matching "pages(\d+)"'
-valid = lambda words:[w for w in words if all(text.count(w) >= n for (_,n,text) in corpus)]
+
+def valid(words):
+    return [w for w in words if all(text.count(w) >= n for (_,n,text) in corpus)]
+    
 words = chars = valid([chr(i) for i in range(256)])
 while True:
     words1 = valid(set([w + c for w in words for c in chars]  
